12ba9792f49cc00a6e2960508e47f43e90ee2e79
BraianUlariaga <ulariagab@gmail.com>
2026-05-20 21:14:38 -0300

    Prueba tecnica

---
+++ Prueba-tecnica/Prueba-tecnica/Entidades/DTO/RecepcionDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Prueba_tecnica.Entidades.DTO
+{
+    public class RecepcionDto
+    {
+        public string SKU { get; set; }
+        public string NombreProducto { get; set; }
+        public int UbicacionId { get; set; }
+        public int Cantidad { get; set; }
+        public decimal ValorDeclaradoUSD { get; set; }
+    }
+}
